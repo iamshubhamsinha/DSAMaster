c0e41158db1dc95bfbb5e3670c678de92ad14113
iamshubhamsinha <imshubhamsinha@gmail.com>
2022-07-13 01:24:06 +0530

    adding arrays

---
+++ DSAMaster/src/arrays/ArraysMainPractice.java
@@ -0,0 +1,13 @@
+package arrays;
+
+import org.apache.logging.log4j.*;
+
+public class ArraysMainPractice {
+
+	private static Logger log = LogManager.getLogger(ArraysMainPractice.class.getName());
+	
+	public static void main(String[] args) {
+		log.info("main method to call topic/question class object and perform ops");
+	}
+
+}
