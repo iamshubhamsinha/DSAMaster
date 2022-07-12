cc005f5d3f8a58b44df3baffdf3f7ad9a8153d85
imshubhamsinha <imshubhamsinha@gmail.com>
2022-07-13 01:01:17 +0530

    Project Setup Commit

---
+++ DSAMaster/src/recursions/FactorialPractice.java
@@ -0,0 +1,12 @@
+package recursions;
+
+public class FactorialPractice {
+	
+	public int getFactorial(int n) {
+		if(n<0)
+			return -1;
+		if(n == 0 || n == 1)
+			return 1;
+		return n * (n-1);
+	}
+}
---
+++ DSAMaster/src/recursions/RecursionsPractice.java
@@ -0,0 +1,22 @@
+package recursions;
+
+import org.apache.logging.log4j.*;
+
+public class RecursionsPractice {
+
+	private static Logger log = LogManager.getLogger(RecursionsPractice.class.getName());
+	
+	public static void main(String[] args) {
+		
+ 		//factorial -->
+		
+//		FactorialPractice factorial = new FactorialPractice();
+//		int n = 3;
+//		int factorialValue = factorial.getFactorial(n);
+//		log.debug(n + "! =" +factorialValue);
+		
+		
+		//
+	}
+
+}
